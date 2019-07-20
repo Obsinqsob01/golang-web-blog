076c865e01563d332696c0150d7fd0a0039e272c
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-07-20 16:13:34 -0500

    Send data to template

--- actions/HomeHandler.go
+++ actions/HomeHandler.go
@@ -1,13 +1,22 @@
 package actions
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
 // HomeHandler home route
 func HomeHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World")
+		tmpl := template.Must(template.ParseFiles("templates/index.html"))
+
+		data := make(map[string]interface{})
+
+		data["Title"] = "GoLang Blog"
+
+		err := tmpl.Execute(w, data)
+		if err != nil {
+			panic(err)
+		}
 	})
 }
